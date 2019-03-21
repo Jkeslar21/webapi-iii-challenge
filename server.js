8df027ae134aa87e35e2b8b283bb28fdbc848dc9
jkeslar21 <diamondroad91@gmail.com>
2019-03-21 12:18:03 -0700

    dynamic port

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ server.use('/api/users', usersRouter);
 // Route Handlers
 server.get('/', (req, res) => {
     res.send(`
-    <h2>Post City Bitch</h2>
+    <h2>Post City</h2>
     <p>Welcome to the Jungle</p>
     `)
 });
